870f9e56efd5c66ec1aa2a7d00ce437f5b39247f
SadnanRashid <sadnan.rashid.07@gmail.com>
2023-02-02 00:39:41 +0600

    update

---
+++ src/Firebase/firebase-config.js
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyA0keQ8EmVRIKtEwoxOWh-wpVUnkqUh4pE",
+  authDomain: "resolute-task.firebaseapp.com",
+  projectId: "resolute-task",
+  storageBucket: "resolute-task.appspot.com",
+  messagingSenderId: "774988444740",
+  appId: "1:774988444740:web:8991af516c40ad88443811",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
