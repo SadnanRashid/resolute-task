8498b456bde77191f15a8f3eb3fae832532a993b
SadnanRashid <sadnan.rashid.07@gmail.com>
2023-02-01 23:10:57 +0600

    update

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,11 +2,9 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">
-        Logo
-      </a>
-      <button className="btn btn-outline-primary ml-auto">Button</button>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light flex justify-content-between px-5">
+      <h1 className="display-6">LOGO</h1>
+      <button className="btn btn-outline-dark ml-auto w-25">Button</button>
     </nav>
   );
 }
